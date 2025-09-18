a33a49480cc3cb3da371024f52961b0cdb15edde
5urafel <surafel5540f@gmail.com>
2025-09-18 15:57:21 +0300

    feat: refactor entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
+import './src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Let's Code"),
-        backgroundColor: Color(0xff6750a4),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print("I am pressed");
-        },
-      ),
-    ),
-  );
-  ;
+  var app = App();
 
   runApp(app);
 }
