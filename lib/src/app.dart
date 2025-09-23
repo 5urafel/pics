f021c52d85519fc5776d81998240e98a44eaa9ae
5urafel <surafel5540f@gmail.com>
2025-09-23 15:30:02 +0300

    feat: add data fetching capabilities

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -8,23 +12,42 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
+
+  fetchData() async {
+    counter++;
+    var response = await http.get(
+      Uri.parse('https://dummyjson.com/products/$counter'),
+      headers: {
+        "Accept": "application/json",
+        "User-Agent": "FlutterApp/1.0", // 👈 this tricks Cloudflare
+      },
+    );
+
+    if (response.statusCode == 200) {
+      // Only decode if it's valid JSON
+      var imageModel = ImageModel.fromJson(json.decode(response.body));
+      setState(() {
+        images.add(imageModel);
+      });
+    } else {
+      print("Request failed: ${response.statusCode}");
+      print("Response body: ${response.body}");
+    }
+  }
 
   @override
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('$counter'),
+        body: ImageList(images),
         appBar: AppBar(
           title: const Text("Let's Code"),
           backgroundColor: Color(0xff6750a4),
         ),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchData,
         ),
       ),
     );
